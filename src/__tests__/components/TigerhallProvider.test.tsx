085e2decb0f8a075cc5b0a9bf42ba6f5a377c4b0
dion-michael <dionagoes@gmail.com>
2024-06-25 07:20:58 +0800

    remove unnecessary files

--- src/__tests__/components/TigerhallProvider.test.tsx
+++ src/__tests__/components/TigerhallProvider.test.tsx
@@ -1,27 +1,5 @@
 import { render, screen } from "@testing-library/react"
-import ProgressBar from "../../components/ProgressBar"
-import SearchResultCard from "../../components/SearchResultCard"
 import TigerhallApolloProvider from "../../components/TigerhallApolloProvider";
-
-const cardData = {
-  "name": "test azure gpt 4 chat api",
-  "image": {
-    "uri": "https://images.staging.tigerhall.io/users/2023-09-13/7f7ec0b5-c32c-4f70-89ed-c7f80925c449-460114c6-a8d7-498c-8541-567e82bc4069.png"
-  },
-  "categories": [
-    {
-      "name": "category Enola"
-    },
-    {
-      "name": "category Loyal"
-    }
-  ],
-  "timeSpentOnByUsers": 0,
-  "length": 679,
-  "hostedBy": [
-    { firstName: 'dion', lastName: 'agoes', company: 'tigerhall' }
-  ]
-};
 
 describe('ProgressBar', () => {
   it('should render correctly', () => {
