5de10f3f10e206d2228c7750f8cd5cff7fb11f4e
dion-michael <dionagoes@gmail.com>
2024-06-25 05:15:22 +0800

    add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,13 @@ function App() {
   return (
     <Box as='section' display="flex" alignItems="center" justifyContent="center">
       <Box maxW={400} width={"100%"} p={12}>
-        <SearchBar onChange={e => debouncedFetch(e.target.value)} />
+        <SearchBar onChange={e => debouncedFetch(e.target.value)} loading={loading} />
         <Flex alignItems="center" justifyContent="center" flexDir="column" paddingY={6}>
           {loading && [...Array(3)].map((x, i) => <LoadingCard key={i} />)}
           {data && data.contentCards.edges.map((data: any) => (
             <SearchResultCard cardData={data} key={data.id} />
           ))}
         </Flex>
-        {loading ? 'loading...' : <pre>{JSON.stringify(data, null, 2)}</pre>}
       </Box>
     </Box>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,18 @@
 import { SearchIcon } from '@chakra-ui/icons';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftElement, Spinner } from '@chakra-ui/react';
 import React from 'react'
 
 interface Props {
   value?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  loading?: boolean;
 }
 
-const SearchBar: React.FC<Props> = (props) => {
+const SearchBar: React.FC<Props> = ({ loading, ...props }) => {
   return (
     <InputGroup>
       <InputLeftElement pointerEvents="none">
-        <SearchIcon color="gray.300" />
+        {loading ? <Spinner /> : <SearchIcon color="gray.300" />}
       </InputLeftElement>
       <Input {...props} focusBorderColor='brand.secondary' type='text' placeholder='search' />
     </InputGroup>
